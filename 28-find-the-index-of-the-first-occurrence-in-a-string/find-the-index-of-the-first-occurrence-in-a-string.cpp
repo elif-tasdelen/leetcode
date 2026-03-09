783b9fa4368c95e2b187c34000374eeeaa9a05b0
Elif Taşdelen <eliftasdelen24@hotmail.com>
2026-03-09 17:37:02 +0300

    Time: 0 ms (100.00%) | Memory: 9.1 MB (44.79%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        //haystack'te arama yapacağımız başlangı. indexleri geziliyor
-     for(int i = 0; i <= haystack.length(); i++){
-        //needle daki karakterleri kontrol etmek için kullanılacak index
-        int j = 0;
-        //karşılaştırma
-        //1)needle ın sonunda mıyız
-        //2)karakterler eşleşiyor olmalı
-        while(j < needle.length() && haystack[i+j] == needle[j]){
-            j++;
-        }
-        //karakterler eşleştikçe j artacak
-        //en son tüm karakterler eşleştiyse başlangıç indexi döndürülür
-        if(j == needle.length()) {
-            return i;
-     }   
-    }
-    return -1;
+        int pos = haystack.find(needle);
+
+        if(pos == string::npos)
+            return -1;
+
+        return pos;
     }
 };
